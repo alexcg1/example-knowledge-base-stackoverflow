ecc95c656a04e7709caa847b63a25f8fd12ecab6
Alex C-G <alexcg@outlook.com>
2022-05-31 13:41:26 +0200

    docs: update

--- frontend/helper.py
+++ frontend/helper.py
@@ -4,7 +4,7 @@ result_template = """### {title}
 
 {text}"""
 
-sidebar_text = "# Introduction\n\nThis chatbot is built using: \n\n - [**DocArray**](https://docarray.jina.ai): The data structure for unstructured data\n- [**Jina**](https://github.com/jina-ai/jina/): Cloud native neural search framework\n- [**JCloud**](https://github.com/jina-ai/jcloud/) -  Simplify deploying and managing Jina projects on Jina Cloud \n- [**Streamlit**](https://streamlit.io/) - frontend\n## Useful links\n\n- [Code repository](https://github.com/alexcg1/example-chatbot)\n - [COVID-QA dataset from Kaggle](https://www.kaggle.com/xhlulu/covidqa/)\n\n## Disclaimer\n\nThis app is intended only as a technical demonstration. **DO NOT TAKE MEDICAL ADVICE FROM STRANGE APPS ON THE INTERNET** (especially ones with outdated datasets)"
+sidebar_text = "# Introduction\n\nThis search engine is built using: \n\n - [**DocArray**](https://docarray.jina.ai): The data structure for unstructured data\n- [**Jina**](https://github.com/jina-ai/jina/): Cloud native neural search framework\n- [**JCloud**](https://github.com/jina-ai/jcloud/) -  Simplify deploying and managing Jina projects on Jina Cloud \n- [**Streamlit**](https://streamlit.io/) - frontend\n## Useful links\n\n- [Code repository](https://github.com/alexcg1/example-knowledge-base-search)\n - [COVID-QA dataset from Kaggle](https://www.kaggle.com/xhlulu/covidqa/)\n\n## Disclaimer\n\nThis app is intended only as a technical demonstration. **DO NOT TAKE MEDICAL ADVICE FROM STRANGE APPS ON THE INTERNET** (especially ones with outdated datasets)"
 
 
 def get_rating(rating_text):
