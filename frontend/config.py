19a1aca9e3cd29d4be7ffe64e5783805ae313677
Alex C-G <alexcg@outlook.com>
2022-06-08 15:52:11 +0200

    docs: fix urls

--- frontend/config.py
+++ frontend/config.py
@@ -1,2 +1,3 @@
 HOST = "grpcs://kbstackoverflow-6b2a47012d.wolf.jina.ai"
 DATABASE_FILE = "../data/answers.sqlite"
+REPO_URL = "https://github.com/alexcg1/example-knowledge-base-stackoverflow"
--- frontend/helper.py
+++ frontend/helper.py
@@ -1,6 +1,6 @@
 from docarray import Document
 from jina import Client
-from config import HOST, DATABASE_FILE
+from config import HOST, DATABASE_FILE, REPO_URL
 
 result_template = """### {title}
 
@@ -8,7 +8,7 @@ result_template = """### {title}
 
 {text}"""
 
-sidebar_text = "# Introduction\n\nThis search engine is built using: \n\n - [**DocArray**](https://docarray.jina.ai): The data structure for unstructured data\n- [**Jina**](https://github.com/jina-ai/jina/): Cloud native neural search framework\n- [**JCloud**](https://github.com/jina-ai/jcloud/) -  Simplify deploying and managing Jina projects on Jina Cloud \n- [**Streamlit**](https://streamlit.io/) - frontend\n## Useful links\n\n- [Code repository](https://github.com/alexcg1/example-knowledge-base-search)\n - [Stack OverFlow R Questions](https://www.kaggle.com/datasets/stackoverflow/rquestions)"
+sidebar_text = f"# Introduction\n\nThis search engine is built using: \n\n - [**DocArray**](https://docarray.jina.ai): The data structure for unstructured data\n- [**Jina**](https://github.com/jina-ai/jina/): Cloud native neural search framework\n- [**JCloud**](https://github.com/jina-ai/jcloud/) -  Simplify deploying and managing Jina projects on Jina Cloud \n- [**Streamlit**](https://streamlit.io/) - frontend\n## Useful links\n\n- [Code repository]({REPO_URL})\n - [Stack OverFlow R Questions](https://www.kaggle.com/datasets/stackoverflow/rquestions)"
 
 
 def get_rating(status):
